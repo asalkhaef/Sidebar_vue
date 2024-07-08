d734d017df85b38c5f9aafa2947113a0277dadb8
asalkhaef <asal.khaef@gmail.com>
2024-07-08 10:59:29 +0330

    Add starter files

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <button class="menu-icon" @click="toogleSideBarState">
+      <img src="./assets/menu-outline.svg" alt="sidebar icon" />
+    </button>
+    <side-bar :isclicked="SideBarIsOpen"></side-bar>
+  </div>
+</template>
+
+<script>
+import SideBar from "./components/SideBar.vue";
+
+export default {
+  data() {
+    return {
+      SideBarIsOpen: false,
+    };
+  },
+  methods: {
+    toogleSideBarState() {
+      console.log(this.SideBarIsOpen);
+      this.SideBarIsOpen = !this.SideBarIsOpen;
+      console.log(this.SideBarIsOpen);
+    },
+  },
+  components: {
+    SideBar,
+  },
+};
+</script>
+
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+.menu-icon {
+  position: absolute;
+  width: 32px;
+  margin: 15px;
+}
+button {
+  background: none;
+  color: inherit;
+  border: none;
+  padding: 0;
+  font: inherit;
+  cursor: pointer;
+  outline: inherit;
+}
+</style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,103 @@
+<template>
+  <aside class="sidebar-container" :class="{ active: isclicked }">
+    <ul v-if="isclicked" class="item-container">
+      <div v-for="item in itemsList" :key="item.name" class="item">
+        <li>{{ item.name }}</li>
+        <div class="item-icon">
+          <img src="../assets/chevron-down-outline.svg" alt="" />
+        </div>
+      </div>
+    </ul>
+  </aside>
+</template>
+
+<script se>
+export default {
+  props: { isclicked: Boolean },
+  data() {
+    return {
+      isOpen: false,
+      itemsList: [
+        {
+          name: "Home",
+          children: [],
+        },
+        {
+          name: "About",
+          children: [],
+        },
+        {
+          name: "Contact",
+          children: [],
+        },
+        {
+          name: "Settings",
+          children: [],
+        },
+      ],
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style scoped>
+button {
+  background: none;
+  color: inherit;
+  border: none;
+  padding: 0;
+  font: inherit;
+  cursor: pointer;
+  outline: inherit;
+}
+ul {
+  list-style-type: none;
+}
+.sidebar-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 300px;
+  min-height: 100vh;
+  background-color: cadetblue;
+  overflow: hidden;
+  transition: all ease 0.5s;
+  transition-property: left;
+  position: fixed;
+  top: 0;
+  left: -300px;
+  z-index: 99;
+}
+
+.item-icon {
+  width: 24px;
+}
+.expanded {
+  width: 250px;
+  align-items: flex-start;
+}
+.item-container {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding: 0 20px;
+  margin-top: 20px;
+  width: inherit;
+}
+.item {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-radius: 10px;
+  padding: 10px;
+  transition: all 0.2s;
+}
+.item:hover {
+  background-color: rgb(137, 180, 182);
+  cursor: pointer;
+}
+.active {
+  left: 0;
+}
+</style>
