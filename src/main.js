bc413d9bee7236c358c6e3b21c5c90d6e14d7ce6
asalkhaef <asal.khaef@gmail.com>
2024-07-22 16:13:19 +0330

    Create router for project

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
+import { createRouter, createWebHistory } from "vue-router";
+import HomePage from "./components/HomePage.vue";
+import AboutPage from "./components/AboutPage.vue";
 
+const route = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: "/", component: HomePage },
+    { path: "/about", component: AboutPage },
+  ],
+});
 createApp(App).mount("#app");
