e0a1d01a88925a2a952d6347b6f18d82c92e616a
asalkhaef <asal.khaef@gmail.com>
2024-07-22 10:39:50 +0330

    Set colors in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bg-color": "var(--bg-color)",
+        "sidebar-bg": "var(--sidebar-bg)",
+        "text-color": "var(--text-color)",
+        "hover-color": "var(--hover-color)",
+      },
+    },
   },
   plugins: [],
 };
