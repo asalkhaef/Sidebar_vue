280923cf8db9d18572cc47650a990d1fd9ce1033
asalkhaef <asal.khaef@gmail.com>
2024-07-23 09:49:46 +0330

    Connect more pages to router

--- src/main.js
+++ src/main.js
@@ -4,12 +4,16 @@ import App from "./App.vue";
 import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "./components/HomePage.vue";
 import AboutPage from "./components/AboutPage.vue";
+import ContactPage from "./components/ContactPage.vue";
+import ProfilePage from "./components/ProfilePage.vue";
 
 const route = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", component: HomePage },
     { path: "/about", component: AboutPage },
+    { path: "/contact", component: ContactPage },
+    { path: "/profile", component: ProfilePage },
   ],
 });
 createApp(App).use(route).mount("#app");
